e54d8e835a542f3364a4ca471eda1043d6d80951
github username <github email address>
2019-09-17 17:46:54 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", addToList)
 });
+
+function addToList(event){ 
+  event.preventDefault()
+  const submissionText = document.querySelector("#new-task-description").value
+  debugger
+  document.querySelector("#tasks").insertAdjacentHTML("beforeend", `<li>${submissionText}<button data-description=${submission}>x</button></li>`)
+}
